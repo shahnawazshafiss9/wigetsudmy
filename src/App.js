bd9929515a99ff6c97b51b2018aa8508d047de88
Shahnawaz Alam <shahnawazshafiss9@gmail.com>
2021-02-10 17:34:00 +0530

    starting vedio 153

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react' 
+import Accordion from './components/Accordion'
+import Search from './components/Search'
+const App = () => {
+  const dataAcorddion = [
+    {
+      'title': 'what is react?',
+      'content': 'React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.'
+    },
+    {
+      'title': 'why use react?',
+      'content': 'Since component logic is written in JavaScript instead of templates, you can easily pass rich data through your app and keep state out of the DOM.'
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container">
+      <Search />
+      {/* <Accordion dataa={dataAcorddion} />  */}
     </div>
   );
 }
