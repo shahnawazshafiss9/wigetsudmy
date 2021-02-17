7e32c0172ecf702fd7605759ed098285cff3c748
Shahnawaz Alam <shahnawazshafiss9@gmail.com>
2021-02-17 18:33:43 +0530

    done secton

--- src/App.js
+++ src/App.js
@@ -3,33 +3,58 @@ import Accordion from './components/Accordion'
 import Search from './components/Search'
 import Dropdown from './components/Dropdown'
 import Translate from './components/Translate'
+import Route from './components/Route'
+import Header from './components/Header'
 const App = () => {
-  // const dataAcorddion = [
-  //   {
-  //     'title': 'what is react?',
-  //     'content': 'React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.'
-  //   },
-  //   {
-  //     'title': 'why use react?',
-  //     'content': 'Since component logic is written in JavaScript instead of templates, you can easily pass rich data through your app and keep state out of the DOM.'
+  const dataAcorddion = [
+    {
+      'title': 'what is react?',
+      'content': 'React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.'
+    },
+    {
+      'title': 'why use react?',
+      'content': 'Since component logic is written in JavaScript instead of templates, you can easily pass rich data through your app and keep state out of the DOM.'
+    }
+  ];
+  const options = [
+    {
+      label: 'The Color Red',
+      value: 'red'
+    },
+    {
+      label: 'The Color Green',
+      value: 'green'
+    },
+    {
+      label: 'The shade of Blue',
+      value: 'blue'
+    },
+  ];
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+  const showAccordian = () => {
+    if (window.location.pathname === '/') {
+      return <Accordion dataa={dataAcorddion} />
+    }
+  };
+  const showList = () => {
+    if (window.location.pathname === '/list') {
+      return <Search />
+    }
+  };
+  // const showDropdown = () => {
+  //   if (window.location.pathname === '/dropdown') {
+  //     return <Dropdown />
   //   }
-  // ]
-  // const options = [
-  //   {
-  //     label: 'The Color Red',
-  //     value: 'red'
-  //   },
-  //   {
-  //     label: 'The Color Green',
-  //     value: 'green'
-  //   },
-  //   {
-  //     label: 'The shade of Blue',
-  //     value: 'blue'
-  //   },
-  // ];
-  // const [selected, setSelected] = useState(options[0]);
-  // const [showDropdown, setShowDropdown] = useState(true);
+  // // };
+  // const showTranslate = () => {
+  //   if (window.location.pathname === '/translate') {
+  //     return <Translate />
+  //   }
+  // };
+  // const showComponent = (route, component) => {
+  //   return window.location.pathname === route ? component : null;
+  // };
 
   return (
     <div className="ui container">
@@ -42,9 +67,29 @@ const App = () => {
         options={options}
       /> : null} */}
       {/* <Accordion dataa={dataAcorddion} />  */}
-      <Translate />
-
-
+      {/* <Translate /> */}
+      {/* {showAccordian()}
+      {showList()}
+      {showDropdown()}
+      {showTranslate()} */}
+      <Header />
+      <Route path="/">
+        <Accordion dataa={dataAcorddion} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown 
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+          />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 }
